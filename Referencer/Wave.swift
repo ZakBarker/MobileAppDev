35d08cff95166288fb4ede82671b0dd065555b60
ZakBarker <zakobama@hotmail.com>
2020-03-09 20:33:00 +1000

    Created Wave Class

---
+++ Referencer/Wave.swift
@@ -0,0 +1,26 @@
+//
+//  Wave.swift
+//  Referencer
+//
+//  Created by Zak Barker on 9/3/20.
+//  Copyright © 2020 Zak Barker. All rights reserved.
+//
+
+import Foundation
+
+class Wave {
+    var name: String
+    var country: String
+    var type: String
+    var left: String
+    var right: String
+    
+    init(name: String, country: String, type: String, left: String, right: String){
+        self.name = name
+        self.country = country
+        self.type = type
+        self.left = left
+        self.right = right
+    }
+    
+}
