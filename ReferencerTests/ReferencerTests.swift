54cf9fa41038ab92042809182c7c7eda8a84ab63
Zak Barker <zakobama@hotmail.com>
2020-03-10 17:41:02 +1000

    Updated tests

--- ReferencerTests/ReferencerTests.swift
+++ ReferencerTests/ReferencerTests.swift
@@ -19,20 +19,24 @@ class ReferencerTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testWaveName() {
+        XCTAssertEqual(wave.name, "South Straddie")
     }
     
-    func testWaveTitles() {
-        XCTAssertEqual(wave.name, "South Straddie")
+    func testWaveCountry() {
         XCTAssertEqual(wave.country, "Australia")
     }
     
-    func textWaveDetails() {
+    func textWaveType() {
         XCTAssertEqual(wave.type, "Beach Break")
+    }
+    
+    func textWaveLeft() {
         XCTAssertEqual(wave.left, "Average")
+    }
+    
+    func textWaveRight() {
         XCTAssertEqual(wave.right, "Sick Barrels")
     }
 
